d8614d63439036a7ba0f6c5b379bd95857d6cd7c
delhomvi <delhommeau.victor@orange.fr>
2022-11-14 16:56:46 +0100

    fonctions sql add

--- scripts_java/ConnectSQLOne.java
+++ scripts_java/ConnectSQLOne.java
@@ -1,4 +1,5 @@
 package scripts_java;
+
 import java.util.*;
 import java.util.ArrayList.*;
 import java.sql.*;  
@@ -8,7 +9,7 @@ javac ConnectSQLOne.java
 
 puur pouvoir être éxécuté le fichier mysql-connector-java-8.0.30.jar doit etre présent
 le lancement se fait :
-java -cp mysql-connector-java-8.0.30.jar ConnectSQLOne 
+java -cp mysql-connector-java-8.0.30.jar ConnectSQLOne.java
 */
 
 class  ConnectSQLOne {
@@ -17,11 +18,15 @@ class  ConnectSQLOne {
     {
         try
         {  
-            Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/","root","root");  
+            Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/zoo","root","Salami72550!");  
             Statement stmt=con.createStatement();  
-            // ResultSet rs=stmt.executeQuery("select * from auteur");  
+            ResultSet rs=stmt.executeQuery("select * from animal");  
             while(rs.next())  
-                System.out.println(rs.getInt(1)+"  "+rs.getString(2));  
+                System.out.println(String.format("%3s | %20s | %3s",rs.getInt(1),rs.getString(2),rs.getInt(3)));  
+            stmt.executeUpdate("select * from espece");
+            while(rs.next())  
+                System.out.println(String.format("%3s | %20s | %3s",rs.getInt(1),rs.getString(2),rs.getInt(3)));  
+
             con.close();  
         }
         catch(Exception e)
---
+++ scripts_java/sqlCommand.java
@@ -0,0 +1,51 @@
+package scripts_java;
+import java.sql.*;  
+
+public class sqlCommand {
+    public static void main(String[] args){
+        try
+        {  
+            // Se connecte à mon sevreur, initialisation des variables
+            Connection con=connectToMyServer();  
+            readFromSQLservThreeColls(con,"SELECT * FROM animal");
+            modSQLTable(con,"INSERT INTO animal(nom,id_espece) VALUES ('titi',1)");
+            readFromSQLservTwoColls(con,"SELECT * FROM animal");
+            finalize(con);
+        }
+        catch(Exception e)
+        { 
+            System.out .println(e);
+        }  
+    }
+
+    public static Connection connectToMyServer() throws SQLException{ 
+        Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/zoo","root","Salami72550!");  
+        return con;
+    }
+
+    public static void finalize(Connection con) throws SQLException{
+        con.close();
+    }
+
+    public static void readFromSQLservTwoColls(Connection con,String query) throws SQLException{
+        Statement stmt=con.createStatement(); 
+        ResultSet rs=stmt.executeQuery(query);
+        while(rs.next())  
+                System.out.println(String.format("%3s | %20s",rs.getInt(1),rs.getString(2))); 
+    }
+
+    public static void readFromSQLservThreeColls(Connection con,String query) throws SQLException{
+        Statement stmt=con.createStatement(); 
+        ResultSet rs=stmt.executeQuery(query);
+        while(rs.next())  
+                System.out.println(String.format("%3s | %20s | %20s",rs.getInt(1),rs.getString(2),rs.getInt(3))); 
+    }
+
+
+    public static void modSQLTable(Connection con,String query) throws SQLException{
+        Statement stmt=con.createStatement(); 
+        stmt.executeUpdate(query); 
+    }
+
+    
+}
