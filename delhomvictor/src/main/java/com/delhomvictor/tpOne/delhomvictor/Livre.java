5e569d33a8fb8bd30fcde8b511ee2c3fc11ebac3
delhomvi <delhommeau.victor@orange.fr>
2022-12-02 13:20:08 +0100

    push

---
+++ delhomvictor/src/main/java/com/delhomvictor/tpOne/delhomvictor/Livre.java
@@ -0,0 +1,19 @@
+package com.delhomvictor.tpOne.delhomvictor;
+
+public class Livre {
+	private String titre;
+	private String auteur;
+
+	public Livre(String titre, String auteur) {
+		this.titre = titre;
+		this.auteur = auteur;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+}
---
+++ delhomvictor/src/main/java/com/delhomvictor/tpOne/delhomvictor/TpOnEvicApplication.java
@@ -0,0 +1,13 @@
+package com.delhomvictor.tpOne.delhomvictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpOnEvicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpOnEvicApplication.class, args);
+	}
+
+}
