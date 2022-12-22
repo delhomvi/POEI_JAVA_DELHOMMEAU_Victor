66cc3b28c9fab4c8f62af1480a1fcc35ac18b2a5
delhomvi <delhommeau.victor@orange.fr>
2022-12-22 14:22:01 +0100

    implement CRUD and database

--- scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/service/TodoService.java
+++ scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/service/TodoService.java
@@ -1,4 +1,57 @@
 package com.example.tpOne.service;
 
-public class TodoService {
+import com.example.tpOne.entity.Todo;
+import com.example.tpOne.interfaces.ITodo;
+import com.example.tpOne.tools.ServiceHibernate;
+import org.hibernate.Session;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService implements ITodo {
+
+    private ServiceHibernate serviceHibernate;
+    private Session session;
+
+    public TodoService(ServiceHibernate serviceHibernate){
+        this.serviceHibernate = serviceHibernate;
+        session = this.serviceHibernate.getSession();
+    }
+
+    @Override
+    public boolean create(Todo o) {
+        session.beginTransaction();
+        session.persist(o);
+        session.getTransaction().commit();
+        return true;
+    }
+
+    @Override
+    public boolean update(Todo o) {
+        session.beginTransaction();
+        session.update(o);
+        session.getTransaction().commit();
+        return true;
+    }
+
+    @Override
+    public boolean delete(Todo o) {
+        session.beginTransaction();
+        session.remove(o);
+        session.getTransaction().commit();
+        return true;
+    }
+
+    @Override
+    public Todo findById(int id) {
+        Todo res = null;
+        res = (Todo) session.get(Todo.class,id);
+        return res;
+    }
+
+    @Override
+    public List<Todo> findAll() {
+        return session.createQuery("SELECT t FROM Todo t", Todo.class).list();
+    }
 }
