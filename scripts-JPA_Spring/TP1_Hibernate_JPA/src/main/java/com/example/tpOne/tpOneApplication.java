2c1df69220303e67343e252225a2ff7723058082
delhomvi <delhommeau.victor@orange.fr>
2022-12-22 14:41:29 +0100

    rename class

--- scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/tpOneApplication.java
+++ scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/tpOneApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemospringApplication {
+public class tpOneApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(DemospringApplication.class, args);
+        SpringApplication.run(tpOneApplication.class, args);
     }
 
 }
