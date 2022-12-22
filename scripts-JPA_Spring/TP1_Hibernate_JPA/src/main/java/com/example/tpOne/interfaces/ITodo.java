bf713478a1fabf15ce5343843632f46c4ca243f7
delhomvi <delhommeau.victor@orange.fr>
2022-12-22 14:17:46 +0100

    add interface

--- scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/interfaces/ITodo.java
+++ scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/interfaces/ITodo.java
@@ -1,4 +1,17 @@
 package com.example.tpOne.interfaces;
 
+import com.example.tpOne.entity.Todo;
+
+import java.util.List;
+
 public interface ITodo {
+    boolean create(Todo o);
+
+    boolean update(Todo o);
+
+    boolean delete(Todo o);
+
+    Todo findById(int id);
+
+    List<Todo> findAll();
 }
