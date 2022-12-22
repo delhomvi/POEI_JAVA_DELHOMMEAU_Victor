42521e553da9bddddad9ae663786e57a29d612ae
delhomvi <delhommeau.victor@orange.fr>
2022-12-22 14:16:15 +0100

    entity done

--- scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/entity/Todo.java
+++ scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/tpOne/entity/Todo.java
@@ -1,4 +1,21 @@
 package com.example.tpOne.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+import java.util.Date;
+
+@Entity
+@Data
 public class Todo {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String desc;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    private boolean state;
+
 }
