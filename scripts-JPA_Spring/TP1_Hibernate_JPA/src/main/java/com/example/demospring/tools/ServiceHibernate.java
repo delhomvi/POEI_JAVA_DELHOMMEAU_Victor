9cdba880442704560392e4ca64b4051e249999dd
delhomvi <delhommeau.victor@orange.fr>
2022-12-22 14:06:53 +0100

    add TPONE

---
+++ scripts-JPA_Spring/TP1_Hibernate_JPA/src/main/java/com/example/demospring/tools/ServiceHibernate.java
@@ -0,0 +1,23 @@
+package com.example.demospring.tools;
+
+
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceHibernate {
+
+    private Session _session;
+
+    public ServiceHibernate() {
+        try {
+            _session = HibernateUtil.getSesssionFactory().openSession();
+        }catch (HibernateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Session getSession() {return HibernateUtil.getSesssionFactory().openSession();}
+}
